f0b6f7668baabc1698f70e7777da65a4c59a0102
JoeMakuta <makutajosue@gmail.com>
2024-04-22 21:32:58 +0200

    Feat : Test Text Comp

--- apps/web/app/[locale]/josh_tests/page.tsx
+++ apps/web/app/[locale]/josh_tests/page.tsx
@@ -1,24 +1,22 @@
-// import { Text } from 'lib/components';
+'use client';
 
 import { clsxm } from '@app/utils';
-import Meta from '@components/layout/Meta';
 import { AddIcon } from 'assets/svg';
+import { withAuthentication } from 'lib/app/authenticator';
 import { useTranslations } from 'next-intl';
 
 const JoshTests = () => {
 	const t = useTranslations();
 	return (
-		<div>
-			<Meta title="string" keywords="string" description="string" />
-			<h1 className={clsxm('font-bold text-5xl text-center')}>{t('JOSHTEST')}</h1>
+		<div className={clsxm('flex')}>
+			<h1 className={clsxm('font-bold text-2xl text-center')}>{t('JOSHTEST')}</h1>
 			{/* <Text className={clsxm('font-bold text-5xl underline text-center')}>
 				<p>Hello world</p>
 			</Text> */}
 			<button className="p-2 rounded-full border-2 border-[#0000001a] dark:border-white">
 				<AddIcon width={24} height={24} className={'dark:stroke-white'} />
-				{/* <AddIcon className="w-6 h-6 text-foreground" /> */}
 			</button>
 		</div>
 	);
 };
-export default JoshTests;
+export default withAuthentication(JoshTests, { displayName: 'JoshTests' });
--- apps/web/lib/layout/auth-layout.tsx
+++ apps/web/lib/layout/auth-layout.tsx
@@ -1,9 +1,9 @@
 import { clsxm } from '@app/utils';
 import { Meta, Text } from 'lib/components';
 import { EverTeamsLogo } from 'lib/components/svgs';
+import { useTranslations } from 'next-intl';
 import Image from 'next/legacy/image';
 import { PropsWithChildren, ReactNode } from 'react';
-import { useTranslations } from 'next-intl';
 import { Footer } from './footer';
 
 type Props = {
